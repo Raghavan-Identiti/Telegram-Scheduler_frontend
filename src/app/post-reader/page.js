1e8008b27c707984f5bd3e85654f62da9c778424
Raghavan-Identiti <raghavan@identitidesign.com>
2025-09-24 20:10:36 +0530

    Post reader added

--- src/app/post-reader/page.js
+++ src/app/post-reader/page.js
@@ -92,7 +92,7 @@ export default function PostReader() {
         {/* Empty State */}
         {!loading && posts.length === 0 && !errorMsg && (
           <p className="mt-4 text-gray-500 text-center">
-            No posts found. Enter details and click "Read Posts".
+            No posts found. Enter details and click &quot; Read Posts &quot;.
           </p>
         )}
       </div>
